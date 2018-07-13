cfcfd85cb0c8b1b976e94c80cff4dc93b991fc72
Carlos Rivera <carlosa.riveram@gmail.com>
2018-07-12 23:14:29 -0400

    Added the maximum number function

---
+++ Algorithm.Logic/DivideConquer/MaximumNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Logic.DivideConquer
+{
+    /// <summary>
+    /// Maximum number class
+    /// </summary>
+    public class MaximumNumber
+    {
+        /// <summary>
+        /// Finds the maximum number in an array
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="seed"></param>
+        /// <returns>The maximum number</returns>
+        public int Find(int[] numbers, int seed = 0)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return 0;
+            if (numbers.Length == 1)
+                return seed;
+
+            int numberSelected = numbers[numbers.Length - 1];
+            int[] remainderArray = new int[numbers.Length - 1];
+
+            Array.Copy(numbers, remainderArray, numbers.Length - 1);
+
+            if (numberSelected > seed)
+                seed = numberSelected;
+
+            return Find(remainderArray, seed);
+
+        }
+    }
+}
---
+++ Algorithm.Test/DivideConquer/MaximumNumberTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm.Logic.DivideConquer;
+
+namespace Algorithm.Test.DivideConquer
+{
+    /// <summary>
+    /// List Item Count 
+    /// </summary>
+    [TestClass]
+    public class MaximumNumberTest
+    {
+        /// <summary>
+        /// List Item Count
+        /// </summary>
+        private readonly MaximumNumber _algorithm;
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MaximumNumberTest()
+        {
+            _algorithm = new MaximumNumber();
+        }
+
+        /// <summary>
+        /// Test Euclid's algorithm's
+        /// </summary>
+        [TestMethod]
+        public void GetTheMaximumNumberOfElements_Gets_ReturnTheMaximumNumber()
+        {
+            int result = _algorithm.Find(new int[] { 1, 1, 1, 5, 4, 2200, 3, 9});
+
+            Assert.IsTrue(result == 2200);
+        }
+    }
+}
