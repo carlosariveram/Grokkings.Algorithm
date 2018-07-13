f426b626c34e1f1818fb563b2fbae885b99d08a1
Carlos Rivera <carlosa.riveram@gmail.com>
2018-07-12 22:34:09 -0400

    Added the array sum D&C approach

---
+++ Algorithm.Logic/DivideConquer/ArraySum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Logic.DivideConquer
+{
+    /// <summary>
+    /// Sums and array
+    /// </summary>
+    public class ArraySum
+    {
+        /// <summary>
+        /// Sums an array of numbers using divide and conquer strategy
+        /// </summary>
+        /// <param name="arrayNumbers"></param>
+        /// <returns></returns>
+        public int Sum(int[] arrayNumbers)
+        {
+            if (arrayNumbers.Length == 0) return 0;
+            if (arrayNumbers.Length == 1) return arrayNumbers[0];
+
+            int[] resultArray = new int[arrayNumbers.Length - 1];
+
+            Array.Copy(arrayNumbers, resultArray, arrayNumbers.Length - 1);
+            return arrayNumbers[arrayNumbers.Length - 1] + Sum(resultArray);
+        }
+    }
+}
---
+++ Algorithm.Test/DivideConquer/ArraySumTest.cs
@@ -0,0 +1,43 @@
+﻿using Algorithm.Logic.DivideConquer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Test.DivideConquer
+{
+    /// <summary>
+    /// Makes and addition of a Sum
+    /// </summary>
+    [TestClass]
+    public class ArraySumTest
+    {
+        /// <summary>
+        /// Algorithm
+        /// </summary>
+        private readonly ArraySum _algorithm;
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ArraySumTest()
+        {
+            _algorithm = new ArraySum();
+        }
+
+
+        /// <summary>
+        /// Sums all the elements of an array
+        /// </summary>
+        [TestMethod]
+        public void SumsAllTheElementsOfTheArray_ArraySum_ReturnsTheTotalOfTheElements()
+        {
+            int result = _algorithm.Sum(new[] { 1, 2, 4, 5 });
+
+            Assert.IsTrue(result == 12);
+        }
+    }
+}
